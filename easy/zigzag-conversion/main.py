1cacf83bd7d0dd14b79bed040fe565bb545615bd
chenxing <chenxing@lianjia.com>
2016-02-15 10:41:55 +0800

    +zigzag-conversion

--- easy/zigzag-conversion/main.py
+++ easy/zigzag-conversion/main.py
@@ -1,3 +1,5 @@
+# -*- coding:utf-8 -*-
+# description: 核心思想：周期，注意处理尾部的元素
 class Solution(object):
     def convert(self, s, numRows):
         """
@@ -5,8 +7,38 @@ class Solution(object):
         :type numRows: int
         :rtype: str
         """
+        if 1 == numRows:
+            return s
+        size = len(s)
         cycle = 2 * numRows - 2
-        return s
+        seq = self.sequence(numRows)
+        #return seq
+        ret = []
+        curr = 0
+        while curr < size:
+            ret.append(s[curr])
+            curr += cycle
+        for curr_left, curr_right in seq:
+            base = 0
+            while base + curr_left < size:
+                ret.append(s[base + curr_left])
+                if base + curr_right >= size:
+                    break
+                ret.append(s[base + curr_right])
+                base += cycle
+        curr = numRows - 1
+        while curr < size:
+            ret.append(s[curr])
+            curr += cycle
+        return ''.join(ret)
+
+    def sequence(self, numRows):
+        #seq = [0]
+        seq = []
+        for i in range(1, numRows - 1):
+            seq.append( (i, 2 * numRows - 2 - i) )
+        #seq.append(numRows - 1)
+        return seq
 
 # input:
 # PAYPALISHIRING
@@ -24,6 +56,13 @@ class Solution(object):
 
 if "__main__" == __name__:
     s = Solution()
+    print s.convert("PAYPALISHIRING", 1)
+    print s.convert("PAYPALISHIRING", 2)
     print s.convert("PAYPALISHIRING", 3)
+    print s.convert("abcdefghijklmnopqrstuvwxyz", 4) # first circle: abcdef
+    print s.convert("abcdefghijklmnopqrstuvwxyz", 5) # abcdefgh
+    print s.convert("abcdefghijklmnopqrstuvwxyz", 6) # abcdefghij
+
+    print s.convert("abcdef", 3)
 
 #TODO: Inverse solution.
