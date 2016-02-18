ca9ca1a201b26e8133c2092472dcbc5a9b606bfb
chenxing <chenxing@lianjia.com>
2016-02-18 22:55:10 +0800

    +bulls-and-cow 使用了list comprehension和2.7才支持的collecctions.Counter做mset之用，可以直接用&做intersect之计算。

---
+++ easy/bulls-and-cows/main_set.py
@@ -0,0 +1,38 @@
+# -*- coding:utf-8 -*-
+# description: 经典的Bull-Cow游戏
+# 算法必须是2趟，第一趟剔除Bulls，剩下的Cow候选再就行比较。
+class Solution(object):
+    def getHint(self, secret, guess):
+        """
+        :type secret: str
+        :type guess: str
+        :rtype: str
+        """
+        #bulls = [val for i, val in enumerate(secret) if secret[i] == guess[i]]
+        #print bulls
+        #print filter(lambda (i, val): val == secret[i], enumerate(guess))
+        #cow_candidates = [(val, guess[i]) for i, val in enumerate(secret) if secret[i] != guess[i]]
+        #print cow_candidate
+
+        bulls = [val for i, val in enumerate(secret) if secret[i] == guess[i]]
+        secret_cow_candidates = [val for i, val in enumerate(secret) if secret[i] != guess[i]]
+        guess_cow_candidates = [val for i, val in enumerate(guess) if secret[i] != guess[i]]
+        #print bulls
+        #print secret_cow_candidates
+        #print guess_cow_candidates
+        cows = set(secret_cow_candidates) & set(guess_cow_candidates)
+        return "%dA%dB" % (len(bulls), len(cows))
+
+if "__main__" == __name__:
+    s = Solution()
+    print s.getHint("1807", "7810")
+    print "1A3B is expected"
+
+    print s.getHint("1123", "0111")
+    print "1A1B is expected"
+
+    print s.getHint("11123", "00111")
+    print "1A2B is expected"
+
+    print s.getHint("1122", "2211")
+    print "0A4B is expected!!"
