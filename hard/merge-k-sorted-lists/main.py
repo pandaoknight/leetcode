b1cc9a8e43db454065993b31c3bf0114eda3da6e
chenxing <chenxing@lianjia.com>
2016-02-29 17:55:48 +0800

    +merge-k-sorted-lists 用了heapq，站在heapq的基础上算法很简单。自己写的random生成测试用例的算法也很赞。然后尝试了一把reduce()，再次明细了一下list.sort()和sorted()的区别

--- hard/merge-k-sorted-lists/main.py
+++ hard/merge-k-sorted-lists/main.py
@@ -3,7 +3,7 @@
 # 思路：这个题比2个的merge要复杂些，关键在于每次不能循环k次遍历链表的列表！
 # 而应该使用最小堆（heap）来解决这个问题。
 #
-# Definition for singly-linked list.
+# Definition for singly-linked _list.
 class ListNode(object):
     def __init__(self, x, next=None):
         self.val = x
@@ -11,11 +11,11 @@ class ListNode(object):
 
     def toList(self):
         curr = self
-        list = []
+        _list = []
         while curr:
-            list.append(curr.val)
+            _list.append(curr.val)
             curr = curr.next
-        return list
+        return _list
 
     @staticmethod
     def genLinkedList(source):
@@ -24,69 +24,68 @@ class ListNode(object):
             l1 = ListNode(i, l1)
         return l1
 
+import heapq
 class Solution(object):
-    def mergeTwoLists(self, l1, l2):
+    def mergeKLists(self, _lists):
         """
-        :type l1: ListNode
-        :type l2: ListNode
+        :type _lists: List[ListNode]
         :rtype: ListNode
         """
-        if not l1:
-            return l2
-        if not l2:
-            return l1
-        if l1.val < l2.val:
-            head = l1
-            l1 = l1.next
-        else:
-            head = l2
-            l2 = l2.next
+        h = []
+        for _list in _lists:
+            if _list:
+                heapq.heappush(h, (_list.val, _list))
+                _list = _list.next
+        #print h
+        if not h:
+            return None
+        val, head = heapq.heappop(h)
+        if head.next:
+            heapq.heappush(h, (head.next.val, head.next))
         curr = head
-        while l1 and l2:
-            if l1.val < l2.val:
-                curr.next = l1
-                l1 = l1.next
-            else:
-                curr.next = l2
-                l2 = l2.next
+        while h:
+            #print h
+            val, node = heapq.heappop(h)
+            #print val
+            curr.next = node
             curr = curr.next
-        if not l1:
-            curr.next = l2
-        else:
-            curr.next = l1
+            if node.next:
+                node = node.next
+                heapq.heappush(h, (node.val, node))
+
         return head
+
+import random
+def randomGenSortedList(limit, _list_num, repeat_probability=0.0):
+    i = 0
+    _list = []
+    for n in xrange(_list_num):
+        _list.append([])
+    while i < limit:
+        num = random.randint(0, _list_num - 1)
+        _list[num].append(i)
+        if repeat_probability <= random.uniform(0, 1.0):
+            i += 1
+    return _list
+
+def randomGenSortedLinkedList(limit, _list_num, repeat_probability=0.0):
+    _lists = randomGenSortedList(limit, _list_num, repeat_probability)
+    return [ListNode.genLinkedList(_list) for _list in _lists]
 
 if "__main__" == __name__:
     s = Solution()
 
+    #print randomGenSortedList(32, 5)
+    #print randomGenSortedList(32, 5, 0.5)
+    #print randomGenSortedLinkedList(32, 5, 0.5)
+
     ## 注意处理空LinkedList！！
-    l1 = ListNode.genLinkedList([])
-    l2 = ListNode.genLinkedList([])
-    print l1
-    print l2
-    print s.mergeTwoLists(l1, l2)
-    print "None is expected"
-
-    l1 = ListNode.genLinkedList([1])
-    l2 = ListNode.genLinkedList([])
-    print l1.toList()
-    print l2
-    print s.mergeTwoLists(l1, l2).toList()
-    print "1 is expected"
-
-    # begin with l1
-    l1 = ListNode.genLinkedList([1, 2])
-    l2 = ListNode.genLinkedList([2])
-    print l1.toList()
-    print l2.toList()
-    print s.mergeTwoLists(l1, l2).toList()
-    print "1 2 2 is expected"
-
-    # begin with l2
-    l1 = ListNode.genLinkedList([2, 3, 4, 5, 6])
-    l2 = ListNode.genLinkedList([2, 7, 8, 9, 10])
-    print l1.toList()
-    print l2.toList()
-    print s.mergeTwoLists(l1, l2).toList()
-    print "2 2 3 4 5 6 7 8 9 10 is expected"
-
+    _lists = randomGenSortedList(0, 5)
+    #_lists = randomGenSortedList(32, 5)
+    #_lists = randomGenSortedList(32, 32)
+    #_lists = randomGenSortedList(32, 5, 0.5)
+    print _lists
+    print reduce(lambda x, y: sorted(x+y), _lists)
+    _list = s.mergeKLists([ListNode.genLinkedList(_list) for _list in _lists])
+    print _list
+    print _list.toList()
