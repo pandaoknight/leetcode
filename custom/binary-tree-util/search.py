33e9b0c2cbb55a9f908b26ec03539f9e998c5750
chenxing <chenxing@lianjia.com>
2016-03-31 23:51:46 +0800

    完成基本的构筑和遍历。

--- custom/binary-tree-util/search.py
+++ custom/binary-tree-util/search.py
@@ -7,3 +7,25 @@ class TreeNode(object):
 
 
 
+if "__main__" == __name__:
+    valList = xrange(2, 200)
+    root = TreeNode(1)
+    nodeList = [root]
+    current = None
+    for i in valList:
+        if not current or current.right:
+            current = nodeList.pop(0)
+            current.left = TreeNode(i)
+            nodeList.append(current.left)
+        else:
+            current.right = TreeNode(i)
+            nodeList.append(current.right)
+
+    levelTravel = [root]
+    while levelTravel:
+        current = levelTravel.pop(0)
+        if current.left:
+            levelTravel.append(current.left)
+        if current.right:
+            levelTravel.append(current.right)
+        print current.val
