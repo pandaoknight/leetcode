6e3685b1875a563a346f0d2d0215184ab7f1bf3b
Xing Chen <chenxing@lianjia.com>
2016-07-11 12:43:19 +0800

    +search-for-range 一把进大头

--- medium/search-for-a-range/main.py
+++ medium/search-for-a-range/main.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 class Solution(object):
     def searchRange(self, nums, target):
         """
@@ -8,28 +9,40 @@ class Solution(object):
         if target < nums[0] or target > nums[-1]:
             return [-1, -1]
         #1 find one
-        start = 0
-        end = len(nums) - 1
+        start = -1
+        end = len(nums)
         middle = (start+end)/2
-        while start < end or target != nums[middle]:
-            if target < nums[middle]:
-                end = middle - 1
+        while target != nums[middle]:
+            if target < nums[middle]: # 比当前小的话只有一种情况结束
+                end = middle
             else:
-                start = middle + 1
+                start = middle
+            if start+1 == end: # 最后都可以在这个阶段收束成一种情况来判断
+                return [-1, -1]
+                break
             middle = (start+end)/2
-        if start >= end and target != nums[start]:
-            return [-1, -1]
-        return None
+        #return middle # 至此这是一个标准的二分查找
+
         #2 find start
-        mid4start = (start+middle)/2
-        while start != mid4start:
-            pass
+        end4start = middle
+        mid = (start+end4start+1)/2 # 这样保证我取到的是ceil
+        while start != (end4start - 1):
+            if target == nums[mid]:
+                end4start = mid
+            else:
+                start = mid
+            mid = (start+end4start+1)/2
+
         #3 find end
-        mid4end = (end+middle)/2
-        while true:
-            if target != nums[mid4end]:
-                end = mid4end
-        return [start, end]
+        start4end = middle
+        mid = (start4end+end)/2 # 这样保证我取到的是ceil
+        while start4end != (end - 1):
+            if target == nums[mid]:
+                start4end = mid
+            else:
+                end = mid
+            mid = (start4end+end)/2
+        return [end4start, start4end]
 
 if "__main__" == __name__:
     s = Solution()
@@ -43,10 +56,10 @@ if "__main__" == __name__:
     print s.searchRange(nums, 6) # -1,-1
     print s.searchRange(nums, 9) # -1,-1
     nums = [5, 5, 5, 5, 7, 7, 7, 7, 7, 7, 7, 8, 8, 10]
-    print s.searchRange(nums, 8) # 3,4
-    print s.searchRange(nums, 7) # 1,2
-    print s.searchRange(nums, 10) # 5,5
-    print s.searchRange(nums, 5) # 0,0
+    print s.searchRange(nums, 8) # 11,12
+    print s.searchRange(nums, 7) # 4,10
+    print s.searchRange(nums, 10) # 13,13
+    print s.searchRange(nums, 5) # 0,3
     print s.searchRange(nums, 3) # -1,-1
     print s.searchRange(nums, 11) # -1,-1
     print s.searchRange(nums, 6) # -1,-1
