7a627e061713fb23e40c4143460c58bdd13d154b
Pannad Chen <pc@chinaepanda.com>
2016-10-08 21:41:21 +0800

    进一步实现Sample

--- neo_easy/binary_tree_travel/sample_of_dfs_bfs.py
+++ neo_easy/binary_tree_travel/sample_of_dfs_bfs.py
@@ -10,6 +10,24 @@ class TreeNode(object):
 class Solution(object):
     stack = []
 
+    def dfs(self, root, drl=None, rdl=None, rld=None):
+        """
+        Depth-first-search
+        深度优先
+        :type root: TreeNode
+        :rtype: rtype of "fo"
+        """
+        if None != drl:
+            drl(root.val)
+        if None != root.left:
+            self.dfs(root.left, drl, rdl, rld)
+        if None != rdl:
+            rdl(root.val)
+        if None != root.right:
+            self.dfs(root.right, drl, rdl, rld)
+        if None != rld:
+            rld(root.val)
+
     def drl(self, root, fo):
         """
         Depth-first-search Preorder
@@ -27,9 +45,18 @@ class Solution(object):
         print root.val
         return 1
 
+def printVal(val, count=[0]):
+    count[0] += 1
+    print "count:%s val:%s" % (count[0], val)
 
 if "__main__" == __name__:
     s = Solution()
     root = TreeNode(1, TreeNode(2, TreeNode(4), TreeNode(5)), TreeNode(3, TreeNode(6), TreeNode(7)))
     #print s.drl.__doc__
     s.drl(root, None)
+    print "============================"
+    s.dfs(root, drl=printVal)
+    print "============================"
+    s.dfs(root, rdl=printVal)
+    print "============================"
+    s.dfs(root, rld=printVal)
