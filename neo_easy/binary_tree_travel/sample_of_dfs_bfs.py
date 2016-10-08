4fb1b59341bac254c995eff8b2f853a85db751c5
work <work@iZ11uaqiuzdZ.(none)>
2016-10-08 11:10:15 +0800

    +实现基本的DFS Sample

--- neo_easy/binary_tree_travel/sample_of_dfs_bfs.py
+++ neo_easy/binary_tree_travel/sample_of_dfs_bfs.py
@@ -10,8 +10,26 @@ class TreeNode(object):
 class Solution(object):
     stack = []
 
+    def drl(self, root, fo):
+        """
+        Depth-first-search Preorder
+        深度优先 先序遍历
+        :type fo: Function Object
+        :rtype: rtype of "fo"
+        """
+        leftResult = None
+        #print root.val
+        if None != root.left:
+            leftResult = self.drl(root.left, fo)
+        #print root.val
+        if None != root.right:
+            rightResult = self.drl(root.right, fo)
+        print root.val
+        return 1
+
 
 if "__main__" == __name__:
     s = Solution()
     root = TreeNode(1, TreeNode(2, TreeNode(4), TreeNode(5)), TreeNode(3, TreeNode(6), TreeNode(7)))
-    print s.dfsF
+    #print s.drl.__doc__
+    s.drl(root, None)
