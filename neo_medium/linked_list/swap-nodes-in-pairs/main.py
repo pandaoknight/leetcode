6d00bae796c4039a2cc02d685b401202883d5371
pandaoknight@ubuntu <pandaoknight@ubuntu.com>
2017-10-23 12:18:32 +0000

    从reverse-nodes-in-k-group抄过来，令k=2即可。但其实这道题可以用更少的变量，因为每次只调换两个值。

---
+++ neo_medium/linked_list/swap-nodes-in-pairs/main.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, x, n = None):
+        self.val = x
+        self.next = n
+
+    def __str__(self):
+        cur = self
+        s = "%s" % cur.val
+        while cur.next:
+            cur = cur.next
+            s += "->%s" % cur.val
+        return s
+
+class Solution(object):
+    def swapPairs(self, head):
+        """
+        :type head: ListNode
+        :type k: int
+        :rtype: ListNode
+        """
+        #if 1 == k or None == head:
+        #    return head
+        k = 2
+
+        # define a fake head
+        fh = ListNode(None)
+        fh.next = head
+        # define 4 cursor and 1 counter for k
+        gc = head  # gc:group-cursor
+        ocr = head  # ocr:operating-cursor-for-reversing
+        ocl = fh  # orl:operating-cursor-for-last-group
+        ocls = None # orls:operating-cursor-for-last-group-start-mark
+        count = 0
+        while gc:
+            if 0 == count % k:
+                ocls = gc
+            gc = gc.next
+            count += 1
+            if 0 == count % k:
+                tmp = ocr.next
+                ocr.next = gc
+                last = ocr
+                ocr = tmp
+                for i in range(k - 2):
+                    tmp = ocr.next
+                    ocr.next = last
+                    last = ocr
+                    ocr = tmp
+                ocl.next = ocr
+                ocr.next = last
+                ocr = gc
+                ocl = ocls
+        return fh.next
+
+if "__main__" == __name__:
+    s = Solution()
+    print ListNode(1, ListNode(2, ListNode(3, ListNode(4, ListNode(5)))))
+    print s.swapPairs(ListNode(1, ListNode(2, ListNode(3, ListNode(4, ListNode(5))))))
+    print s.swapPairs(ListNode(1, ListNode(2, ListNode(3, ListNode(4, ListNode(5))))))
+
+    print s.swapPairs(ListNode(1, ListNode(2)))
+    print s.swapPairs(ListNode(1, ListNode(2, ListNode(3, ListNode(4)))))
+
+    print s.swapPairs(ListNode(1, ListNode(2, ListNode(3, ListNode(4, ListNode(5))))))
+    print s.swapPairs(ListNode(1, ListNode(2, ListNode(3, ListNode(4, ListNode(5))))))
+    print s.swapPairs(ListNode(1, ListNode(2, ListNode(3, ListNode(4, ListNode(5))))))
