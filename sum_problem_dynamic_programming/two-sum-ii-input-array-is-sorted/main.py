de13eced676bc98bc35e8019bbfe9784417b9f16
pandaoknight@ubuntu <pandaoknight@ubuntu.com>
2017-11-19 05:00:02 +0000

    实现了，但是性能为最后10%。可能还是在于从头删除，降低集合量，容易被最坏情况拉低时间。

--- sum_problem_dynamic_programming/two-sum-ii-input-array-is-sorted/main.py
+++ sum_problem_dynamic_programming/two-sum-ii-input-array-is-sorted/main.py
@@ -10,14 +10,65 @@ class Solution(object):
         """
         if len(nums) < 2:
             return None
+
+        count = -1
         while nums:
-            
+            a = nums.pop(0)
+            count += 1
+            index, lnums, rnums = self.sortedListBinarySplit(nums, target - a)
+            if None != index:
+                return [count, count + index + 1]
+            nums = lnums
 
         return None
 
+    def sortedListBinarySplit(self, nums, target):
+        """
+        :type nums: list[int]
+        :type target: int
+        :rtype index: None or int
+        :rtype lnums: list[int]
+        :rtype rnums: list[int]
+        """
+        if nums[0] >= target:
+            if nums[0] == target:
+                return 0, [], nums[1:]
+            else:
+                return None, [], nums
+        if nums[-1] <= target:
+            if nums[-1] == target:
+                return len(nums)-1, nums[:-1], []
+            else:
+                return None, nums, []
+
+        l, r = 0, len(nums)-1
+        while l+1 != r:
+            m = (l+r)/2
+            if nums[m] == target:
+                return m, nums[:m], nums[m+1:]
+            if nums[m] > target:
+                r = m
+            else:
+                l = m
+        index, lnums, rnums = None, nums[:r], nums[r:]
+        return index, lnums, rnums
 
 
 if "__main__" == __name__:
+    test_s = Solution()
+    print test_s.sortedListBinarySplit([2, 3, 7 ,11, 15], 0)
+    print test_s.sortedListBinarySplit([2, 3, 7 ,11, 15], 16)
+    print test_s.sortedListBinarySplit([2, 3, 7 ,11, 15], 2)
+    print test_s.sortedListBinarySplit([2, 3, 7 ,11, 15], 15)
+    print test_s.sortedListBinarySplit([2, 3, 7 ,11, 15], 7)
+    print test_s.sortedListBinarySplit([2, 3, 7 ,11, 15], 8)
+    print test_s.sortedListBinarySplit([2, 3, 7 ,11, 15], 11)
+    print "==========================="
+    print test_s.sortedListBinarySplit([2,], 2)
+    print test_s.sortedListBinarySplit([2, 2, 2, 2, 2], 2)
+    print test_s.sortedListBinarySplit([2, 2,], 2)
+    print test_s.sortedListBinarySplit([2, 2, 2, 2, 2], 2)
+
     s = Solution()
     print s.twoSum([2, 7], 9)
     print s.twoSum([2, 7 ,11, 15], 9)
